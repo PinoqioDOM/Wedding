3703e5c0d94f1ca45c623627bcdfc51add4fabe6
PinoqioDOM <alkhanishvilit@gmail.com>
2026-09-01 23:46:08 +0400

    feat: Create Loading tsx

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="min-h-[60vh] grid place-items-center">
+      <div className="animate-pulse text-ink-700/60 text-sm tracking-wide">
+        იტვირთება...
+      </div>
+    </div>
+  );
+}
