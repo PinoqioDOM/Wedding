813ff58f6c7622851e9d4944ac81c001680bd484
PinoqioDOM <alkhanishvilit@gmail.com>
2026-09-06 17:24:38 +0400

    added invation gate

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import InvitationGate from "@/components/InvitationGate";
 import localFont from "next/font/local";
 
 export const metadata: Metadata = {
@@ -18,9 +19,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ka" className={davitGuramishvili.variable}>
       <body>
-        <Navbar />
-        <main className="min-h-[calc(100vh-8rem)]">{children}</main>
-        <Footer />
+        <InvitationGate>
+          <Navbar />
+          <main className="min-h-[calc(100vh-8rem)]">{children}</main>
+          <Footer />
+        </InvitationGate>
       </body>
     </html>
   );
